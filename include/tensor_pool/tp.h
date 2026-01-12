96608b82544b10e26e587cfaea53d7ef10f053e2
Darryl Gamroth <dgamroth@rubus.ca>
2026-01-12 15:03:02 -0800

    feat: add umbrella client header

---
+++ include/tensor_pool/tp.h
@@ -0,0 +1,23 @@
+#ifndef TENSOR_POOL_TP_H
+#define TENSOR_POOL_TP_H
+
+#include "tensor_pool/tp_client.h"
+#include "tensor_pool/tp_consumer.h"
+#include "tensor_pool/tp_control.h"
+#include "tensor_pool/tp_control_adapter.h"
+#include "tensor_pool/tp_control_poller.h"
+#include "tensor_pool/tp_discovery_client.h"
+#include "tensor_pool/tp_driver_client.h"
+#include "tensor_pool/tp_error.h"
+#include "tensor_pool/tp_log.h"
+#include "tensor_pool/tp_metadata_poller.h"
+#include "tensor_pool/tp_producer.h"
+#include "tensor_pool/tp_progress_poller.h"
+#include "tensor_pool/tp_qos.h"
+#include "tensor_pool/tp_shm.h"
+#include "tensor_pool/tp_tensor.h"
+#include "tensor_pool/tp_types.h"
+#include "tensor_pool/tp_uri.h"
+#include "tensor_pool/tp_version.h"
+
+#endif
